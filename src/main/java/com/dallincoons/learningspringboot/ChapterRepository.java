2e01acd92d54642210b82ada1f37a37c80dcee0f
Dallin <dallinis@hotmail.com>
2018-07-24 06:42:23 -0700

    initial commit

---
+++ src/main/java/com/dallincoons/learningspringboot/ChapterRepository.java
@@ -0,0 +1,10 @@
+package com.dallincoons.learningspringboot;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface ChapterRepository
+        extends ReactiveCrudRepository<Chapter, String> {
+
+}
